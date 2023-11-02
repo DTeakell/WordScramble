3f5635d08818abeef7c747ac567ee30b2dc3c5ed
Dillon Teakell <teakelld@icloud.com>
2023-11-02 13:17:59 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dillon Teakell on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
